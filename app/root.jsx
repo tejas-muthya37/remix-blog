3a720ad5ab60e466054326e47a680e439beabaa4
tejas-muthya37 <55662912+tejas-muthya37@users.noreply.github.com>
2022-07-28 13:30:34 +0530

    Feat: Loaders and Actions

--- app/root.jsx
+++ app/root.jsx
@@ -63,3 +63,16 @@ function Layout({ children }) {
     </>
   );
 }
+
+export function ErrorBoundary({ error }) {
+  console.log(error);
+
+  return (
+    <Document>
+      <Layout>
+        <h1>Error!</h1>
+        <p>{error.message}</p>
+      </Layout>
+    </Document>
+  );
+}
--- app/routes/posts/new.jsx
+++ app/routes/posts/new.jsx
@@ -1,8 +1,46 @@
+import { redirect } from "@remix-run/node";
+import { Link } from "@remix-run/react";
+
+export const action = async ({ request }) => {
+  const form = await request.formData();
+  const title = form.get("title");
+  const body = form.get("body");
+
+  const fields = { title, body };
+
+  // @todo - submit to DB
+
+  return redirect("/posts");
+};
+
 function NewPost() {
   return (
-    <div>
-      <h1>New post!</h1>
-    </div>
+    <>
+      <div className="page-header">
+        <h1>New Post</h1>
+        <Link to="/posts" className="btn btn-reverse">
+          Back
+        </Link>
+      </div>
+
+      <div className="page-content">
+        <form method="POST">
+          <div className="form-control">
+            <label htmlFor="title">Title</label>
+            <input type="text" name="title" id="title" />
+          </div>
+
+          <div className="form-control">
+            <label htmlFor="body">Post Body</label>
+            <textarea name="body" id="body" />
+          </div>
+
+          <button className="btn btn-block" type="submit">
+            Add Post
+          </button>
+        </form>
+      </div>
+    </>
   );
 }
 
